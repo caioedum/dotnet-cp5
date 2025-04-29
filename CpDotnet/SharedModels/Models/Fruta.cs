0874ad6253998cd40c558c94817fa814cf969d85
Caio Eduardo <caio3martins@gmail.com>
2025-04-29 11:02:34 -0300

    feat: cria as models compartilhadas

---
+++ CpDotnet/SharedModels/Models/Fruta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels.Models
+{
+    public class Fruta
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime DataRegistro { get; set; }
+    }
+}
---
+++ CpDotnet/SharedModels/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels.Models
+{
+    public class Usuario
+    {
+        public string? NomeCompleto { get; set; }
+        public string? Endereco { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+        public DateTime DataRegistro { get; set; }
+    }
+}
